61706baa828de0d3437d91aa092d26d14b4fd608
arif <arif@themascoteers.com>
2024-04-11 21:02:06 +1000

    done

---
+++ evaluator.py
@@ -0,0 +1,91 @@
+from ds_manager import DSManager
+from datetime import datetime
+import os
+from algorithm_creator import AlgorithmCreator
+from sklearn.metrics import r2_score, mean_squared_error
+import math
+import pandas as pd
+import my_utils
+
+
+class Evaluator:
+    def __init__(self, tasks):
+        self.tasks = tasks
+        self.filename = os.path.join("results","results.csv")
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w') as file:
+                file.write("algorithm,rows,columns,time,target_size,final_size,"
+                           "accuracy_train,accuracy_test,"
+                           "rmse_train,rmse_test,"
+                           "selected_features\n")
+
+    def evaluate(self):
+        for task in self.tasks:
+            print(task)
+            dataset = task["dataset"]
+            target_feature_size = task["target_feature_size"]
+            algorithm_name = task["algorithm"]
+            dataset = DSManager(dataset=dataset)
+            if self.is_done(algorithm_name, dataset, target_feature_size):
+                print("Done already. Skipping.")
+                continue
+            elapsed_time, accuracy_original, rmse_original, \
+                accuracy_reduced_train, rmse_reduced_train, \
+                accuracy_reduced_test, rmse_reduced_test, \
+                final_indices, selected_features = \
+                self.do_algorithm(algorithm_name, dataset, target_feature_size)
+
+
+            with open(self.filename, 'a') as file:
+                file.write(
+                    f"{algorithm_name},{dataset.count_rows()},"
+                    f"{dataset.count_features()},{round(elapsed_time,2)},{target_feature_size},{final_indices},"
+                    f"{accuracy_reduced_train},{accuracy_reduced_test},"
+                    f"{rmse_reduced_train},{rmse_reduced_test},"
+                    f"{';'.join(str(i) for i in selected_features)}\n")
+
+    def is_done(self,algorithm_name,dataset,target_feature_size):
+        df = pd.read_csv(self.filename)
+        if len(df) == 0:
+            return False
+        rows = df.loc[
+            (df['algorithm'] == algorithm_name) &
+            (df['rows'] == dataset.count_rows()) &
+            (df['columns'] == dataset.count_features()) &
+            (df['target_size'] == target_feature_size)
+        ]
+        return len(rows) != 0
+
+    def do_algorithm(self, algorithm_name, dataset, target_feature_size):
+        X_train, y_train, X_test, y_test = dataset.get_train_test_X_y()
+        print(f"X_train,X_test: {X_train.shape} {X_test.shape}")
+        #_, _, accuracy_original, rmse_original = Evaluator.get_metrics(algorithm_name, X_train, y_train, X_test, y_test)
+        algorithm = AlgorithmCreator.create(algorithm_name, X_train, y_train, target_feature_size)
+        start_time = datetime.now()
+        selected_features = algorithm.fit()
+        elapsed_time = (datetime.now() - start_time).total_seconds()
+        X_train_reduced = algorithm.transform(X_train)
+        X_test_reduced = algorithm.transform(X_test)
+        accuracy_reduced_train, rmse_reduced_train, accuracy_reduced_test, rmse_reduced_test = \
+            Evaluator.get_metrics(algorithm_name, X_train_reduced, y_train, X_test_reduced, y_test)
+        return elapsed_time, 0, 0, \
+            accuracy_reduced_train, rmse_reduced_train, \
+            accuracy_reduced_test, rmse_reduced_test, X_test_reduced.shape[1], selected_features
+
+    @staticmethod
+    def get_metrics(algorithm_name, X_train, y_train, X_test, y_test):
+        metric_evaluator = my_utils.get_metric_evaluator(algorithm_name, X_train)
+        metric_evaluator.fit(X_train, y_train)
+
+        y_pred = metric_evaluator.predict(X_train)
+        accuracy_train = round(accuracy_score(y_train, y_pred), 2)
+        rmse_train = round(math.sqrt(mean_squared_error(y_train, y_pred)), 2)
+
+        y_pred = metric_evaluator.predict(X_test)
+        accuracy_test = round(accuracy_score(y_test, y_pred), 2)
+        rmse_test = round(math.sqrt(mean_squared_error(y_test, y_pred)), 2)
+
+        print(f"accuracy train {accuracy_train}")
+        print(f"accuracy test {accuracy_test}")
+
+        return accuracy_train, rmse_train, accuracy_test, rmse_test
